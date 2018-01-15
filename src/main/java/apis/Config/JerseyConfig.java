92b23445b15a885c937bd332e131b18cf1e90b34
Prashant Dharane <pdharane@gmail.com>
2018-01-15 14:53:09 +0530

    Integrated jersey

---
+++ src/main/java/apis/Config/JerseyConfig.java
@@ -0,0 +1,12 @@
+package apis.Config;
+
+import apis.controllers.GreetingController;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig(){
+        register(GreetingController.class);
+    }
+}
--- src/main/java/apis/controllers/GreetingController.java
+++ src/main/java/apis/controllers/GreetingController.java
@@ -3,17 +3,25 @@ package apis.controllers;
 import apis.mapper.UserMapper;
 import apis.models.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Component
+@Path("/greeting")
 public class GreetingController {
 
     @Autowired
     UserMapper userMapper;
 
 
-    @RequestMapping("/greetings")
+    @GET
+    @Produces("application/json")
+    @Path("/greet")
     public Users greetings() {
         return userMapper.findUserById(1);
     }
